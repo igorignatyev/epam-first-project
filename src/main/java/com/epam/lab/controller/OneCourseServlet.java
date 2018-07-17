dea08a66874da9585716e37ac912ccc61738eab9
David Guyo <David_Guyo@epam.com>
2018-07-17 13:11:01 +0300

    15_course-rest: Add Course REST including servlets and jsp

---
+++ src/main/java/com/epam/lab/controller/OneCourseServlet.java
@@ -0,0 +1,85 @@
+package com.epam.lab.controller;
+
+import com.epam.lab.dao.CourseDaoImpl;
+import com.epam.lab.dao.GenericDao;
+import com.epam.lab.dao.TeacherDaoImpl;
+import com.epam.lab.entity.Course;
+import com.epam.lab.entity.Teacher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class OneCourseServlet extends HttpServlet {
+    private static final GenericDao<Course> courseDao = new CourseDaoImpl();
+    private static final GenericDao<Teacher> teacherDao = new TeacherDaoImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = 0;
+
+        try {
+            id = Integer.parseInt(req.getParameter("id"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        Course course = courseDao.find(id);
+        List<Teacher> teacherList = teacherDao.findAll();
+
+        String name = course.getName();
+        String description = course.getDescription();
+        int teacherId = course.getTeacherId();
+
+        req.setAttribute("id", id);
+        req.setAttribute("name", name);
+        req.setAttribute("description", description);
+        req.setAttribute("teacherId", teacherId);
+        req.setAttribute("teachers", teacherList);
+
+        try {
+            req.getRequestDispatcher("/one_course.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)  {
+        int id = 0;
+
+        try {
+            id = Integer.parseInt(req.getParameter("id"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        String action = req.getParameter("action");
+
+        if ("editCourse".equals(action)) {
+            try {
+                String name = req.getParameter("name");
+                String description = req.getParameter("description");
+                int teacherId = Integer.parseInt(req.getParameter("teacherId"));
+                courseDao.update(id, new Course(id, name, description, teacherId)); //teacherId
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if ("deleteCourse".equals(action)) {
+            courseDao.delete(id);
+        }
+
+        try {
+            resp.sendRedirect("/courses");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
